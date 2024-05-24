03f0f19cccd863415227afcbadaf86007222c622
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-05-24 19:53:15 +0100

    Ex 3, create GetItem and SetItem methods

--- MyGenericClass/Guarda3.cs
+++ MyGenericClass/Guarda3.cs
@@ -19,5 +19,29 @@ namespace MyGenericClass
             g3 = default(T);
         }
         
+        public T GetItem(int i)
+        {
+            switch (i)
+            {
+                case 0: return g1;
+                case 1: return g2;
+                case 2: return g3;
+                default: throw new IndexOutOfRangeException();                
+            }
+        }
+
+        public void SetItem(int i, T item)
+        {
+            switch (i)
+            {
+                case 0: g1 = item;
+                    break;
+                case 1: g2 = item;
+                    break;
+                case 2: g3 = item;
+                    break;
+                default: throw new IndexOutOfRangeException();                
+            }
+        }
     }
 }
